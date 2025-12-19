f9ed35d135dd583a61078ca014dc85c3b51641a3
Carl Majneri <carl.majneri@me.com>
2025-12-19 23:04:28 +0100

    Fix Docker installation and authentication
    - Enable .env file loading in bootstrap.php
    - Use env() variables for database config in app.default.php
    - Fix password hash in seed.sql for admin user
    - Add try-catch for DebugKit plugin loading
    - Remove DEBUG from docker-compose.yml (use .env instead)

--- config/app.default.php
+++ config/app.default.php
@@ -248,16 +248,16 @@ return [
             'className' => 'Cake\Database\Connection',
             'driver' => 'Cake\Database\Driver\Mysql',
             'persistent' => false,
-            'host' => 'localhost',
+            'host' => env('MYSQL_URL', 'localhost'),
             /*
              * CakePHP will use the default DB port based on the driver selected
              * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
              * the following line and set the port accordingly
              */
             //'port' => 'non_standard_port_number',
-            'username' => 'my_app',
-            'password' => 'secret',
-            'database' => 'my_app',
+            'username' => env('MYSQL_USERNAME', 'edubank'),
+            'password' => env('MYSQL_PASSWORD', ''),
+            'database' => env('MYSQL_DATABASE', 'edubank'),
             /*
              * You do not need to set this flag to use full utf-8 encoding (internal default since CakePHP 3.6).
              */
